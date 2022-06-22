19334da2f9808082dd328332d17be4d87e54a984
Sathish Ramasami <103182679+sathish0143@users.noreply.github.com>
2022-06-22 18:29:22 +0530

    Add files via upload

---
+++ No2.js
@@ -0,0 +1,55 @@
+//Reverse a linked list in groups of given size
+console.clear()
+var head;
+	class Node {
+			constructor(val) {
+				this.data = val;
+				this.next = null;
+			}
+		}
+	function reverse(head , k) {
+		if (head == null)
+			return null;
+		var current = head;
+		var next = null;
+		var prev = null;
+		var count = 0;
+		while (count < k && current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+			count++;
+		}
+		if (next != null)
+			head.next = reverse(next, k);
+		return prev;
+	}
+	function push(new_data) {
+		new_node = new Node(new_data);
+		new_node.next = head;
+		head = new_node;
+	}
+	function linkedList() {
+		temp = head;
+		while (temp != null) {
+			console.log(temp.data);
+			temp = temp.next;
+		}
+	}
+		push(9);
+		push(8);
+		push(7);
+		push(6);
+		push(5);
+		push(4);
+		push(3);
+		push(2);
+		push(1);
+		console.log("Given Linked List");
+		linkedList();
+		head = reverse(head, 3);
+		console.log("Reversed list");
+		linkedList();
+
+
