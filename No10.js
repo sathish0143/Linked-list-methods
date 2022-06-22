6c539c346f487370d3cd9a924db330a9f14fc14e
Sathish Ramasami <103182679+sathish0143@users.noreply.github.com>
2022-06-22 19:10:45 +0530

    Add files via upload

---
+++ No10.js
@@ -0,0 +1,41 @@
+//Implement a queue using the stack data structure
+console.clear();
+class Queue{
+     
+constructor()
+{
+    this.s1 = [];
+    this.s2 = [];
+}
+enQueue(x)
+{
+    while (this.s1.length != 0)
+    {
+        this.s2.push(this.s1.pop());
+    }
+    this.s1.push(x);
+    while (this.s2.length != 0)
+    {
+        this.s1.push(this.s2.pop());
+        //s2.pop();
+    }
+}
+deQueue()
+{
+    if (this.s1.length == 0)
+    {
+        document.write("Q is Empty");
+    }
+    let x = this.s1[this.s1.length - 1];
+    this.s1.pop();
+    return x;
+}
+}
+let q = new Queue();
+q.enQueue(1);
+q.enQueue(2);
+q.enQueue(3);
+ 
+console.log(q.deQueue());
+console.log(q.deQueue());
+console.log(q.deQueue());
